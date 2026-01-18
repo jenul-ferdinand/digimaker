865cbe9c5b5356607e6c4d0c211c66b78fa938a4
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-18 18:32:17 +1100

    add title section component

---
+++ packages/frontend/src/app/components/dumb/lesson-preview/title-section/title-section.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-title-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './title-section.component.html',
+  styleUrl: './title-section.component.scss'
+})
+export class TitleSectionComponent {
+  @Input({ required: true }) topic!: string;
+  @Input({ required: true }) project!: string;
+  @Input({ required: true }) description!: string;
+}
