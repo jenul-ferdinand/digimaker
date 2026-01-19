90340daca718eae1cd8a756cf3ac727f49abc1c5
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-19 13:26:45 +1100

    add try it out component

---
+++ packages/frontend/src/app/components/dumb/lesson-preview/try-it-out/try-it-out.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-try-it-out',
+  standalone: true,
+  imports: [],
+  templateUrl: './try-it-out.component.html',
+  styleUrl: './try-it-out.component.scss'
+})
+export class TryItOutComponent {
+  @Input({ required: true }) tryItOutData!: Array<string>;
+}
