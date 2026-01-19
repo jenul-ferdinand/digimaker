b2b07022892015bff0bef4865804c9c4e9edbba3
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-20 02:49:01 +1100

    update add-your-code comp to support alternative section format

--- packages/frontend/src/app/components/dumb/lesson-preview/add-your-code/add-your-code.component.ts
+++ packages/frontend/src/app/components/dumb/lesson-preview/add-your-code/add-your-code.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { StepWithImage, StepsWithCodeBlock } from '@digimakers/core';
+import { StepWithImage, StepsWithCodeBlock, MultipleStepsWithCodeBlock } from '@digimakers/core';
 import { PrismHighlightDirective } from '@directives/prism-highlight.directive';
 
 @Component({
@@ -10,18 +10,33 @@ import { PrismHighlightDirective } from '@directives/prism-highlight.directive';
   templateUrl: './add-your-code.component.html',
 })
 export class AddYourCodeComponent {
-  @Input({ required: true }) data!: StepWithImage[] | StepsWithCodeBlock;
+  @Input({ required: true }) data!:
+    | StepWithImage[]
+    | StepsWithCodeBlock
+    | MultipleStepsWithCodeBlock;
   @Input({ required: true }) programmingLanguage!: string;
 
-  get isStepWithImageArray(): boolean {
-    return Array.isArray(this.data);
+  get isStepWithImage(): boolean {
+    return Array.isArray(this.data) && this.data.every((item) => 'step' in item);
+  }
+
+  get isMultipleStepsWithCodeBlock(): boolean {
+    return Array.isArray(this.data) && this.data.every((item) => 'steps' in item);
+  }
+
+  get isStepsWithOneCodeBlock(): boolean {
+    return !Array.isArray(this.data) && this.data !== null && 'steps' in this.data;
   }
 
   get stepsWithImages(): StepWithImage[] {
-    return this.isStepWithImageArray ? (this.data as StepWithImage[]) : [];
+    return this.isStepWithImage ? (this.data as StepWithImage[]) : [];
   }
 
   get stepsWithCodeBlock(): StepsWithCodeBlock | null {
-    return !this.isStepWithImageArray ? (this.data as StepsWithCodeBlock) : null;
+    return this.isStepsWithOneCodeBlock ? (this.data as StepsWithCodeBlock) : null;
+  }
+
+  get multipleStepsWithCodeBlock(): MultipleStepsWithCodeBlock {
+    return this.isMultipleStepsWithCodeBlock ? (this.data as MultipleStepsWithCodeBlock) : [];
   }
 }
