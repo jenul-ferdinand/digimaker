055fc772bb9a0ee33bb10f93b8f5158e4012bcd7
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-21 15:28:48 +1100

    remove useless old getters

--- packages/frontend/src/app/components/dumb/lesson-preview/add-your-code/add-your-code.component.ts
+++ packages/frontend/src/app/components/dumb/lesson-preview/add-your-code/add-your-code.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { StepWithImage, StepsWithCodeBlock, MultipleStepsWithCodeBlock } from '@digimakers/core';
+import { StepWithImage, StepsWithCodeBlock } from '@digimakers/core/schemas';
 import { PrismHighlightDirective } from '@directives/prism-highlight.directive';
 
 @Component({
@@ -12,31 +12,23 @@ import { PrismHighlightDirective } from '@directives/prism-highlight.directive';
 export class AddYourCodeComponent {
   @Input({ required: true }) data!:
     | StepWithImage[]
-    | StepsWithCodeBlock
-    | MultipleStepsWithCodeBlock;
+    | StepsWithCodeBlock[]
+    | null;
   @Input({ required: true }) programmingLanguage!: string;
 
   get isStepWithImage(): boolean {
     return Array.isArray(this.data) && this.data.every((item) => 'step' in item);
   }
 
-  get isMultipleStepsWithCodeBlock(): boolean {
-    return Array.isArray(this.data) && this.data.every((item) => 'steps' in item);
-  }
-
-  get isStepsWithOneCodeBlock(): boolean {
-    return !Array.isArray(this.data) && this.data !== null && 'steps' in this.data;
-  }
-
   get stepsWithImages(): StepWithImage[] {
     return this.isStepWithImage ? (this.data as StepWithImage[]) : [];
   }
 
-  get stepsWithCodeBlock(): StepsWithCodeBlock | null {
-    return this.isStepsWithOneCodeBlock ? (this.data as StepsWithCodeBlock) : null;
+  get isStepsWithCodeBlockArray(): boolean {
+    return Array.isArray(this.data) && this.data.every((item) => 'steps' in item);
   }
 
-  get multipleStepsWithCodeBlock(): MultipleStepsWithCodeBlock {
-    return this.isMultipleStepsWithCodeBlock ? (this.data as MultipleStepsWithCodeBlock) : [];
+  get stepsWithCodeBlockArray(): StepsWithCodeBlock[] {
+    return this.isStepsWithCodeBlockArray ? (this.data as StepsWithCodeBlock[]) : [];
   }
 }
