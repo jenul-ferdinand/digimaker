79b10e9846dc65d142654d82d0aeb51fd888b507
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-21 15:28:55 +1100

    update import for challenge section

--- packages/frontend/src/app/components/dumb/lesson-preview/challenge-section/challenge-section.component.ts
+++ packages/frontend/src/app/components/dumb/lesson-preview/challenge-section/challenge-section.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Challenge } from '@digimakers/core';
+import { Challenge } from '@digimakers/core/schemas';
 import { PrismHighlightDirective } from '@directives/prism-highlight.directive';
 
 @Component({
