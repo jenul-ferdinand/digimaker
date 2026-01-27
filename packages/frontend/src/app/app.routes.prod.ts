667c49fa74454b7d148f43680a147db3cb458f18
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-27 20:41:34 +1100

    don't build lesson preview in production

---
+++ packages/frontend/src/app/app.routes.prod.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { PrintComponent } from '@components/smart/print/print.component';
+
+export const routes: Routes = [{ path: 'print', component: PrintComponent }];
