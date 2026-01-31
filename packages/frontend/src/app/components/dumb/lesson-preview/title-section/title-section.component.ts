36cb16cc531544ea8b3a6672bb7fe35dcff53af2
jenul-ferdinand <jenul15ferdinand@gmail.com>
2026-01-31 18:14:15 +1100

    update title-section description to render markdown

--- packages/frontend/src/app/components/dumb/lesson-preview/title-section/title-section.component.ts
+++ packages/frontend/src/app/components/dumb/lesson-preview/title-section/title-section.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { MarkdownPipe } from '../../../../pipes/markdown.pipe';
 
 @Component({
   selector: 'app-title-section',
   standalone: true,
-  imports: [],
+  imports: [MarkdownPipe],
   templateUrl: './title-section.component.html',
   styleUrl: './title-section.component.scss',
 })
